2c73007e29e0473b9c41fb7970ba27f9e3c0e401
bttf <adnanchowdhury88@gmail.com>
2013-11-11 20:09:55 -0500

    adding vagrant config

---
+++ manifests/base.pp
@@ -0,0 +1,33 @@
+$packages = [ 'mercurial', 'git', 'vim' ]
+$gems = [ 'rspec-puppet', 'puppetlabs_spec_helper', 'rake' ]
+
+package {$packages:
+  ensure => installed,
+  before => Exec['dotfiles'],
+}
+
+package {$gems:
+  ensure   => 'installed',
+  provider => 'gem',
+}
+
+exec {'dotfiles':
+  cwd     => '/home/vagrant/',
+  creates => '/home/vagrant/dotfiles',
+  command => 'git clone --recursive https://github.com/bttf/dotfiles',
+  logoutput => 'on_failure',
+  path    => [ '/bin', '/usr/bin', '/usr/local/bin' ],
+}
+
+file {'/home/vagrant/.vimrc':
+  ensure  => 'link',
+  target  => '/home/vagrant/dotfiles/vimrc',
+  require => Exec['dotfiles'],
+}
+
+file {'/home/vagrant/.vim':
+  ensure  => 'link',
+  target  => '/home/vagrant/dotfiles/vimfiles',
+  require => Exec['dotfiles'],
+}
+
