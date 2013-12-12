ada88fc17c86a9b9e5bf477bb31e24d20c9fb879
bttf <adnanchowdhury88@gmail.com>
2013-12-12 00:46:12 +0000

    i'm an idiot

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,4 +1,4 @@
-$packages = [ 'mercurial', 'git', 'vim', 'make']
+$packages = [ 'mercurial', 'git', 'vim' ]
 $gems = [ 'rspec-puppet', 'puppetlabs_spec_helper', 'rake' ]
 
 package {$packages:
@@ -38,42 +38,5 @@ file {'/home/vagrant/.vim':
   ensure  => 'link',
   target  => '/home/vagrant/dotfiles/vimfiles',
   require => Exec['dotfiles'],
-}
-
-file { 'downloads_dir':
-  ensure => directory,
-  path   => '/home/vagrant/downloads',
-  mode   => '0755',
-  owner  => 'vagrant',
-  group  => 'vagrant',
-}
-
-$nix_path = [ '/bin', '/usr/bin', '/usr/local/bin' ]
-
-exec { 'get_node_tar':
-  user    => 'vagrant',
-  cwd     => '/home/vagrant/downloads',
-  command => 'wget http://nodejs.org/dist/v0.10.22/node-v0.10.22.tar.gz',
-  path    => $nix_path,
-  creates => '/home/vagrant/downloads/node-v0.10.22.tar.gz',
-  require => File['downloads_dir'],
-}
-
-exec { 'untar_node':
-  user    => 'vagrant',
-  cwd     => '/home/vagrant/downloads',
-  command => 'tar zxvf node-v0.10.22.tar.gz',
-  path    => $nix_path,
-  creates => '/home/vagrant/downloads/node-v0.10.22',
-  require => Exec['get_node_tar'],
-  notify  => Exec['make_node'],
-}
-
-exec { 'make_node':
-  cwd         => '/home/vagrant/downloads/node-v0.10.22',
-  path        => $nix_path,
-  command     => './configure && make && make install',
-  refreshonly => true,
-  require     => [ Exec['untar_node'], Package[$packages] ],
 }
 
